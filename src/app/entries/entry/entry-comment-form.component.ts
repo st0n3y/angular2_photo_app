77722cd5929272ab692eb551f37378a8a3759273
User <user@iMac-CodeClan201.local>
2017-03-17 15:54:51 +0000

    Adds POST request for comments

--- src/app/entries/entry/entry-comment-form.component.ts
+++ src/app/entries/entry/entry-comment-form.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { EntryService } from '../shared/entry.service';
 
 @Component({
     selector: 'app-entry-comment-form',
@@ -8,4 +10,20 @@ import { Component } from '@angular/core';
 export class EntryCommentFormComponent {
     name: string = "";
     comment: string = "";
+    @Input() entryId: number;
+    @Output() onCommentAdded = new EventEmitter<{name: string; comment: string;}>();
+    @ViewChild('commentForm') commentForm: NgForm;
+
+    constructor(private entryService: EntryService) {
+
+    }
+
+    onSubmit(commentForm: NgForm) {
+        let comment = { name: this.name, comment: this.comment };
+        this.entryService.addComment(this.entryId, comment)
+            .then(() => {
+                this.onCommentAdded.emit(comment);
+                this.commentForm.resetForm();
+            });
+    }
 }
